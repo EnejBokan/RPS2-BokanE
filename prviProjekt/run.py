e47c27d66071e17c9d7236d224e6d05cdd6f46fa
EnejBokan <bokanenej@gmail.com>
2026-09-08 10:10:27 +0000

    venv ne dela

--- prviProjekt/run.py
+++ prviProjekt/run.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 appAdress = "0.0.0.0"
 appPort = 5000
 
-app.cofig["DEBUG"] = True
+@app.route("/", methods = ["GET", "POST"])
+def helloWorld ():
+    return render_template("index.html")
+
+app.config["DEBUG"] = True
 app.run(host = appAdress, port = appPort)
 
+"""
 @app.route("/")
 def hello_world():
     return "Hello 2.Ri!"
+"""
