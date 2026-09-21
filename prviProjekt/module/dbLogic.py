c66259d26f4485a84e1436085e15c83193554be1
Enej <bokanenej@gmail.com>
2026-09-21 13:46:52 +0200

    db

---
+++ prviProjekt/module/dbLogic.py
@@ -0,0 +1,17 @@
+from module import dbConfig
+
+def getAll (param = ""):
+    
+    try:
+        mydb = dbConfig.dbConnect()
+        cursor = mydb.cursor()
+        
+        cursor.close()
+        mydb.close()
+        return True
+        
+    except:
+        return False
+    
+    finally:
+        pass
